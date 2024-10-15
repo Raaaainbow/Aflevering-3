9e0e673d9ce70c2321cee587a9737c318eefa68f
Sebastian Taylor <Seba7204@gmail.com>
2024-10-15 18:30:49 +0200

    prime factor logik opdateret

--- PrimeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors.java
@@ -1,37 +1,28 @@
 import java.util.Scanner;
+import java.math.*;
 
 public class PrimeFactors {
-    public static void main(String[] args) {
-        FactorFinder(new Scanner(System.in));
-    }
+  public static void main(String[] args) {
+      FactorFinder(new Scanner(System.in));
+  }
 
-  	public static void FactorFinder(Scanner console) {
-      int counter = 0;
-      int[] factors = new int[counter];
-      System.out.print("Enter integer greater than 1 (0 to terminate): ");
-      	long n = console.nextLong();
-        if (!(n%2==0)) {
-          notDivisibleBy2(n, factors, counter);
-        } else {
-          divisibleBy2(n, factors, counter);
+  public static void FactorFinder(Scanner console) {
+    System.out.print("Enter integer greater than 1 (0 to terminate): ");
+      long n = console.nextLong();
+      if (n % 2 == 0) {
+        while (n % 2 == 0) {
+          n /= 2;
+          System.out.print("2 ");
         }
       }
-      public static void notDivisibleBy2 (long n, int factors[], int counter) {
-        if (!(n%2==0)) {
-          for (int i = 2; i>= Math.sqrt(n); i++) {
-            if (!(n%i==0)) {
-              i++;
-              counter++;
-            } else {
-              
-            }
-          } 
-      }
-      public static void divisibleBy2 (long n, int factors[], int counter) {
-        while (n%2==0) {
-          n = n/2;
+      else if (!(n % 2 == 0)) {
+        for (int i = 3; i <= Math.sqrt(n); i++) {
+          if (n % i == 0) {
+            n /= i;
+            System.out.print(i + " ");
+          }
         }
-      }
+      } 
   }
 }
 // mens n/2 så del tallet med 2
