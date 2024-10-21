f475193ecfbae57f8d6810fec9c71651c92de3d4
Raaaainbow <Sebastian@tjr.dk>
2024-10-21 14:48:26 +0200

    PrimeFactors Virker, mangler lidt formattering

--- PrimeFactors/PrimeFactors.java
+++ PrimeFactors/PrimeFactors.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
-import java.math.*;
 
 public class PrimeFactors {
   public static void main(String[] args) {
-      FactorFinder(new Scanner(System.in));
+    FactorFinder(new Scanner(System.in));
   }
 
   public static void FactorFinder(Scanner console) {
     System.out.print("Enter integer greater than 1 (0 to terminate): ");
-      long n = console.nextLong();
-      if (n % 2 == 0) {
-        while (n % 2 == 0) {
-          n /= 2;
-          System.out.print("2 ");
+    long n = console.nextLong();
+    System.out.print("List of prime factors:");
+      while (n % 2 == 0) {
+        n /= 2;
+        System.out.print("2, ");
+      }
+      for (int i = 3; i <= Math.sqrt(n); i += 2) {
+        while (n % i == 0) {
+          System.out.print(i + ", ");
+          n /= i;
         }
       }
-      else if (!(n % 2 == 0)) {
-        for (int i = 3; i <= Math.sqrt(n); i++) {
-          if (n % i == 0) {
-            n /= i;
-            System.out.print(i + " ");
-          }
-        }
-      } 
-  }
+      if (n > 2) {
+        System.out.print(n);
+      }
+    } 
 }
-// mens n/2 så del tallet med 2
-// hvis tallet ikke kan deles med 2, så check alle tal større end 2 indtil kvadartroden af n
