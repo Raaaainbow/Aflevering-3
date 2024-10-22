7dc934b15547abd757e2d30ce8f6ad27153ce8d3
Raaaainbow <Sebastian@tjr.dk>
2024-10-22 13:31:04 +0200

    Comments added to prime PrimeFactors

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -8,21 +8,27 @@ public class PrimeFactors {
   public static void FactorFinder(Scanner console) {
     System.out.print("\nEnter integer greater than 1 (0 to terminate): ");
     long n = console.nextLong();
-    if (n==0) {
+    if (n == 0) {
       System.out.println();
     } else {
       System.out.print("\nList of prime factors:");
+
+      // Devides n by 2 while the remainder is 0
       while (n % 2 == 0) {
         n /= 2;
         System.out.print("2, ");
       }
+
+      // check if remainder is 0 for greater than 2 to sqaure root of number
       for (int i = 3; i <= Math.sqrt(n); i += 2) {
         while (n % i == 0) {
           System.out.print(i + ", ");
           n /= i;
         }
       }
-      if (n > 2) {
+
+      // prints numbers that weren't caught in previous step
+      if (n < 2) {
         System.out.print(n + " ");
       }
       FactorFinder(new Scanner(System.in));
