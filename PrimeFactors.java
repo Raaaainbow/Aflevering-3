22a6045366be58b1b269fe6e8b22c2bf65b638db
Raaaainbow <Sebastian@tjr.dk>
2024-10-22 20:51:18 +0200

    PrimeFactors returns if inpurt is 0

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -6,32 +6,36 @@ public class PrimeFactors {
   }
 
   public static void FactorFinder(Scanner console) {
-    System.out.print("\nEnter integer greater than 1 (0 to terminate): ");
+    System.out.print("Enter integer greater than 1 (0 to terminate): ");
     long n = console.nextLong();
+    System.out.print("\b");
     if (n == 0) {
-      System.out.println();
+
+      // Returns nothing, so the program ends
+      return;
     } else {
-      System.out.print("\nList of prime factors:");
+      System.out.print("List of prime factors:");
 
       // Devides n by 2 while the remainder is 0
       while (n % 2 == 0) {
         n /= 2;
-        System.out.print("2, ");
+        System.out.print("2 ");
       }
 
       // check if remainder is 0 for greater than 2 to sqaure root of number
       for (int i = 3; i <= Math.sqrt(n); i += 2) {
         while (n % i == 0) {
-          System.out.print(i + ", ");
+          System.out.print(", " + i );
           n /= i;
         }
       }
 
       // prints numbers that weren't caught in previous step
-      if (n < 2) {
-        System.out.print(n + " ");
+      if (n >= 2) {
+        System.out.print(n);
       }
-      FactorFinder(new Scanner(System.in));
+      System.out.print("\n\n");
     }
+    FactorFinder(new Scanner(System.in));
     } 
 }
