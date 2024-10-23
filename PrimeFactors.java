d7d75c686091255310986a7d60c01a16020d8d1d
Raaaainbow <Sebastian@tjr.dk>
2024-10-23 10:30:32 +0200

    Bug fixed, second forloop variable "i" changed from int to long

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -9,6 +9,9 @@ public class PrimeFactors {
     System.out.print("Enter integer greater than 1 (0 to terminate): ");
     long n = console.nextLong();
     System.out.print("\b");
+    System.out.println("DEBUGGING THIS IS n: " + n);
+    System.out.println("DEBUGGING THIS IS sqrt(n): " + Math.sqrt(n));
+
     if (n == 0) {
 
       // Returns nothing, so the program ends
@@ -17,16 +20,15 @@ public class PrimeFactors {
       System.out.print("List of prime factors:");
 
       // Devides n by 2 while the remainder is 0
-      // I think the bug with 9201111169755555649 comes from here, instead of printing "2, " it should print the fraction
       while (n % 2 == 0) {
         n /= 2;
-        System.out.print("2, ");
+        System.out.print(" 2,");
       }
 
       // check if remainder is 0 for greater than 2 to sqaure root of number
-      for (int i = 3; i <= Math.sqrt(n); i ++) {
+      for (long i = 3; i <= Math.sqrt(n); i ++) {
         while (n % i == 0) {
-          System.out.print(i + ", ");
+          System.out.print(" " + i + ",");
           n /= i;
         }
       }
