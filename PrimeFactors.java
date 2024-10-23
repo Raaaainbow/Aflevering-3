d176643282ff0970868065b7ac65baf7397a9236
slayervictor <victor.r2002@gmail.com>
2024-10-23 10:43:22 +0200

    Primefactors Fencepost Problem DONE

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -9,34 +9,41 @@ public class PrimeFactors {
     System.out.print("Enter integer greater than 1 (0 to terminate): ");
     long n = console.nextLong();
     System.out.print("\b");
-    System.out.println("DEBUGGING THIS IS n: " + n);
-    System.out.println("DEBUGGING THIS IS sqrt(n): " + Math.sqrt(n));
-
+    String output = "";
+    
     if (n == 0) {
 
       // Returns nothing, so the program ends
       return;
     } else {
-      System.out.print("List of prime factors: ");
+      System.out.print("List of prime factors:");
 
       // Devides n by 2 while the remainder is 0
       while (n % 2 == 0) {
         n /= 2;
-        System.out.print(" 2,");
+        output += " 2,";
       }
 
       // check if remainder is 0 for greater than 2 to sqaure root of number
       for (long i = 3; i <= Math.sqrt(n); i ++) {
         while (n % i == 0) {
-          System.out.print(" " + i + ",");
+          output += " " + i + ",";
           n /= i;
         }
       }
 
       // prints numbers that weren't caught in previous step
       if (n >= 2) {
-        System.out.print(n);
+        output += " " + n;
       }
+
+      // fjerner komma til sidst hvis der opstår fencepost problem
+      if (output.charAt(output.length()-1) == ',') {
+        output = output.substring(0, output.length()-2);
+      }
+      
+      // Printer resultatet
+      System.out.println(output);
       System.out.print("\n\n");
     }
     FactorFinder(new Scanner(System.in));
