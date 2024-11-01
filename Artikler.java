3be3bc97bf3e655a89384e97cb5b2d6ef49ff5df
Sophiasol24 <sophiasolstein@gmail.com>
2024-11-01 16:08:12 +0100

    a

--- Artikler.java
+++ Artikler.java
@@ -2,101 +2,101 @@ public class Artikler {
     public static void main(String[] args) {
         ArtikelTest s = new ArtikelTest();
         s.main(args);
-    }
-}
-
-class Forlag {
-    private String navn; 
-    private String sted; 
-
-    public Forlag (String navn, String sted) { // Konstruktør med navn og sted som parametre 
-        this.navn = navn; 
-        this.sted = sted; 
-    }
-    
-    public String toString() {
-        return "Forlaget " + navn + ", " + sted; 
-    } 
-}
-
-class Tidsskrift {
-    private String titel; 
-    private Forlag forlag; 
-    private String issn;
-
-    public Tidsskrift (String titel, Forlag forlag, String issn) { // Tidsskrift med titel som parameter 
-            this.titel = titel; 
-            this.issn = issn; 
-            this.forlag = forlag; 
-        }
-    public void setIssn (String issn) {
-        this.issn = issn; 
-    }
-    public void setForlag (Forlag forlag) {
-        this.forlag = forlag; 
-    }
-   
-    public String getTitel() { // så vi kan få titlen i to String
-        return titel; 
-    }
-
-public String toString() {
-    return titel + ", fra " + forlag + ", " + (issn == "" ? "ISSN-nummeret kendes ikke" : issn); //Hvis issn-nummer ikke angives skriver vi at det ikke kendes
-}
-}
-class Artikel {
-    private String [] forfattere; 
-    private String titelArtikel; 
-    private Tidsskrift tidsskrift; 
-    private String [] referenceliste; 
-
-    public Artikel (String titelArtikel, String [] forfattere, Tidsskrift tidsskrift, String [] referenceliste) { // hvis man vil sætte referencelist til start
-    this.forfattere  = forfattere; 
-    this.titelArtikel = titelArtikel; 
-    this.referenceliste = referenceliste;
-    this.tidsskrift = tidsskrift; 
-    }
-
-    public Artikel (String titelArtikel, String [] forfattere, Tidsskrift tidsskrift) { // hvis referencelisten skal være tom
-        this.forfattere  = forfattere; 
-        this.titelArtikel = titelArtikel;
-        this.tidsskrift = tidsskrift;  
-    }
-    public void setReferencer (String [] referenceliste) {
-    this.referenceliste = referenceliste; 
-
-    }
-    public String[]getReferencer () {
-        return this.referenceliste; 
-    
-        }
-
-    public boolean compareReferences(Artikel andenArtikel) {
-        for (int i = 0 ; i < referenceliste.length ; i++) {
-            if (andenArtikel.titelArtikel == referenceliste[i]) {
-                return true;
             }
         }
-        return false; 
-    
+        
+        class Forlag {
+            private String navn; 
+            private String sted; 
+        
+            public Forlag (String navn, String sted) { // Konstruktør med navn og sted som parametre 
+                this.navn = navn; 
+                this.sted = sted; 
+            }
+            
+            public String toString() {
+                return "Forlaget " + navn + ", " + sted; 
+            } 
         }
-
-    public String toString() {
-        String temp = ""; // for loop til forfatterne i artiklen 
-        for (int i = 0; i < forfattere.length; i++) {
-            temp += forfattere[i]; 
-            if (i < forfattere.length-1) {
-                temp += " & ";
+        
+        class Tidsskrift {
+            private String titel; 
+            private Forlag forlag; 
+            private String issn;
+        
+            public Tidsskrift (String titel, Forlag forlag, String issn) { // Tidsskrift med titel som parameter 
+                    this.titel = titel; 
+                    this.issn = issn; 
+                    this.forlag = forlag; 
+                }
+            public void setIssn (String issn) {
+                this.issn = issn; 
+            }
+            public void setForlag (Forlag forlag) {
+                this.forlag = forlag; 
+            }
+           
+            public String getTitel() { // så vi kan få titlen i to String
+                return titel; 
+            }
+        
+        public String toString() {
+            return titel + ", fra " + forlag + ", " + (issn == "" ? "ISSN-nummeret kendes ikke" : issn); //Hvis issn-nummer ikke angives skriver vi at det ikke kendes
+        }
+        }
+        class Artikel {
+            private String [] forfattere; 
+            private String titelArtikel; 
+            private Tidsskrift tidsskrift; 
+            private String [] referenceliste; 
+        
+            public Artikel (String titelArtikel, String [] forfattere, Tidsskrift tidsskrift, String [] referenceliste) { // hvis man vil sætte referencelist til start
+            this.forfattere  = forfattere; 
+            this.titelArtikel = titelArtikel; 
+            this.referenceliste = referenceliste;
+            this.tidsskrift = tidsskrift; 
+            }
+        
+            public Artikel (String titelArtikel, String [] forfattere, Tidsskrift tidsskrift) { // hvis referencelisten skal være tom
+                this.forfattere  = forfattere; 
+                this.titelArtikel = titelArtikel;
+                this.tidsskrift = tidsskrift;  
+            }
+            public void setReferencer (String [] referenceliste) {
+            this.referenceliste = referenceliste; 
+        
+            }
+            public String[]getReferencer () {
+                return this.referenceliste; 
+            
+                }
+        
+            public boolean compareReferences(Artikel andenArtikel) {
+                for (int i = 0 ; i < referenceliste.length ; i++) {
+                    if (andenArtikel.titelArtikel == referenceliste[i]) {
+                        return true;
+                    }
+                }
+                return false; 
+            
+                }
+        
+            public String toString() {
+                String temp = ""; // for loop til forfatterne i artiklen 
+                for (int i = 0; i < forfattere.length; i++) {
+                    temp += forfattere[i]; 
+                    if (i < forfattere.length-1) {
+                        temp += " & ";
+                    }
+                }
+                temp += ": \"" + titelArtikel +"\" " + tidsskrift.getTitel(); // efter alle forfatterne kommer ":" og bagefter tidsskrift
+                return temp;
             }
         }
-        temp += ": \"" + titelArtikel +"\" " + tidsskrift.getTitel(); // efter alle forfatterne kommer ":" og bagefter tidsskrift
-        return temp;
-    }
-}
-
-
+        
+        
 class ArtikelTest {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Forlag UP = new Forlag ("University Press", "Denmark"); 
         Tidsskrift Tidsskrift1 = new Tidsskrift ("Journal of Logic", UP, ""); 
         Tidsskrift Tidsskrift2 = new Tidsskrift ("Brain", UP, "");
@@ -110,5 +110,5 @@ class ArtikelTest {
         System.out.println(a);
         System.out.println(b);
         System.out.println(a.compareReferences(b) ? "Artikel A refererer til Artikel B" : "Artikel A refererer ikke til Artikel B");
-    }
+        }
 }
